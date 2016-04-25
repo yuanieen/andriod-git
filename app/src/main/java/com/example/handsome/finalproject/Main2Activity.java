8a5cde4024be50a8c2ccfe51203bee149deeb566
yuanieen <yuanieen@gmail.com>
2016-04-25 17:44:20 +0800

    1 comment

--- app/src/main/java/com/example/handsome/finalproject/Main2Activity.java
+++ app/src/main/java/com/example/handsome/finalproject/Main2Activity.java
@@ -26,7 +26,7 @@ public class Main2Activity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-                
+
             }
         });
     }
